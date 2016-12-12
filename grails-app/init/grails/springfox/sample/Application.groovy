84d249ac7635d4da9538d01cb12aecc5084b70bc
dilipkrish <dilipkrish@users.noreply.github.com>
2016-12-12 12:02:23 +0530

    Added dependency on the springfox-grails

--- grails-app/init/grails/springfox/sample/Application.groovy
+++ grails-app/init/grails/springfox/sample/Application.groovy
@@ -3,18 +3,21 @@ package grails.springfox.sample
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import springfox.documentation.grails.GrailsConfiguration
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @EnableSwagger2
+@Import(GrailsConfiguration.class)
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
 
     @Bean
-    public Docket api(){
+     Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(MetaClass)
     }
